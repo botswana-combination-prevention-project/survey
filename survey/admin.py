cb0176c96d2c30cbcb1019047bde917b236fcfc8
erikvw <ew2789@gmail.com>
2016-12-13 20:30:18 +0200

    add admin urls, boots into admin

--- survey/admin.py
+++ survey/admin.py
@@ -9,5 +9,5 @@ from .admin_site import survey_admin
 @admin.register(Survey, site=survey_admin)
 class SurveyAdmin (ModelAdminBasicMixin, admin.ModelAdmin):
 
-    list_display = ('survey_name', 'survey_abbrev', 'chronological_order',
+    list_display = ('survey_name', 'survey_slug', 'chronological_order',
                     'datetime_start', 'datetime_end')
