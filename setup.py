a7431df8d895a3a6eb4a46323570159ff59377dd
Coulson <ckgathi@bhp.org.bw>
2017-03-11 09:42:55 +0200

    Finished release 0.1.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='survey',
-    version='0.1dev0',
+    version='0.1.0',
     author=u'Botswana-Harvard AIDS Institute',
     author_email='ew2789@gmail.com',
     packages=find_packages(),
