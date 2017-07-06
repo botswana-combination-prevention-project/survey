6e3c9e4e27549906a4eabaf722b3b8ab4e971b01
erikvw <ew2789@gmail.com>
2017-07-06 16:42:19 +0200

    accept surveys in test load

--- survey/tests/load_test_surveys.py
+++ survey/tests/load_test_surveys.py
@@ -7,28 +7,30 @@ class LoadTestSurveysError(Exception):
 
 
 def load_test_surveys(load_count=None, load_all=None,
-                      current_survey_index=None, register_current=None):
-    """Load surveys into site_surveys manually.
+                      current_survey_index=None, register_current=None,
+                      survey_schedules=None, group_name=None, constants=None):
+    """Loads survey_schedules into site_surveys manually.
     """
-
+    survey_schedules = (survey_schedules
+                        or [survey_one, survey_two, survey_three])
+    group_name = group_name or 'test_survey'
     if load_all:
-        load_count = 3
+        load_count = len(survey_schedules)
     elif not load_count:
         load_count = 1
-    elif 0 < load_count > 3:
+    elif 0 < load_count > len(survey_schedules):
         raise LoadTestSurveysError(f'Invalid load count. Got {load_count}.')
 
     if not current_survey_index:
         current_survey_index = 0
-    elif current_survey_index and 0 < current_survey_index > 2:
+    elif current_survey_index and 0 < current_survey_index > (len(survey_schedules) - 1):
         raise LoadTestSurveysError(
             f'Invalid current_survey_index. Got {current_survey_index}.')
 
     register_current = True if register_current is None else register_current
 
-    survey_schedules = [survey_one, survey_two, survey_three]
     for survey_schedule in survey_schedules:
-        survey_schedule.group_name = 'test_survey'
+        survey_schedule.group_name = group_name
         survey_schedule.current = False
         for survey in survey_schedule.surveys:
             survey.current = False
@@ -37,9 +39,9 @@ def load_test_surveys(load_count=None, load_all=None,
     site_surveys.loaded = False
     site_surveys.current_survey_schedules = []
     site_surveys.current_surveys = []
+    site_surveys.constants = constants or {}
 
-    for index, survey_schedule in enumerate(
-            [survey_one, survey_two, survey_three]):
+    for index, survey_schedule in enumerate(survey_schedules):
         if index <= load_count - 1:
             site_surveys.register(survey_schedule)
 
